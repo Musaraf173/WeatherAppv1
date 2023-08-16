9464f7da8526922230aee86750d159843d8acfd6
Musaraf Hussain <87618339+Musaraf173@users.noreply.github.com>
2023-08-16 20:16:38 +0600

    Add files via upload

---
+++ card.php
@@ -0,0 +1,49 @@
+<div class="main-container"> 
+<form class="search" action="index.php" method="get">
+    
+    <div class="input-group mb-3">
+      <input type="text" name="city_name" class="form-control" placeholder="Enter City Name" aria-label="Enter City Name" aria-describedby="Enter City Name">
+      <button class="btn btn-primary" type="submit" name="submit_button"value="Go"id="button-addon2">Go</button>
+    </div>
+</form>    
+<div class=" card-group w-80 ">
+
+        <?php for($i=0;$i<=39;$i+=8): 
+            $main = $decoded->list[$i]->weather[0]->main;
+            $temp = $decoded->list[$i]->main->temp;
+            $temp_min = $decoded->list[$i]->main->temp_min;
+            $temp_max = $decoded->list[$i]->main->temp_max;
+            $temp_feelsLike = $decoded->list[$i]->main->feels_like;
+            $icon = $decoded->list[$i]->weather[0]->icon;
+            $epoch=$decoded->list[$i]->dt;
+            $time=new DateTime("@$epoch");
+            $wind_speed = $decoded->list[$i]->wind->speed;
+            date_default_timezone_set('Asia/Dhaka');
+            $cTime = date('h:i A');
+            $city_name=$decoded->city->name;
+            $cod=$decoded->cod;
+            // echo $cod;
+            // echo $i;
+            ?>
+<?php if($cod == '200'):?>
+            <div class="card c-card ">
+                <img class="card-img-top" style="width=30px"; src="https://openweathermap.org/img/wn/<?php echo $icon ?>@2x.png" alt="Card image cap">
+                <div class="card-body">
+                    <hr>
+                    <p class="card-text time"><?php echo $time->format('d/m/D')." , ". $city_name ?></p>
+                    <h5 class="card-title"><span style="color:#00ffaa">Condition : <?php echo $main ?> </span></h5> 
+                    <br>
+                    <div class="card-border">
+                    <p class="card-text">Current Temperature :<span style="color:"> <?php echo $temp ?>°C </span></p>
+                    <p class="card-text">Min Temperature : <?php echo $temp_min ?>°C</p>
+                    <p class="card-text">Max Temperature : <?php echo $temp_max ?>°C</p>
+                    <p class="card-text">Feels like : <?php echo $temp_feelsLike ?>°C</p>
+                    <p class="card-text">Wind Speed : <?php echo $wind_speed ?>km/h</p>
+                    <p class="card-text"><small class="text-muted">Last updated at <?php echo $cTime ?> </small></p> </div>
+
+                </div>
+            </div>
+<?php endif ;?>
+    <?php endfor; ?>
+    </div>
+</div>
---
+++ header.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" href="styles.css">
+    <title>Weather Forecast</title>
+</head>
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+    <a class="navbar-brand" href="index.php"><span style="color:#00ffaa;">Weather </span>Forecast</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+    
+    </ul>
+  </div>
+</nav>
+
